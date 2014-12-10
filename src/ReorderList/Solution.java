90b4fbc414e42ad7ca437b770a409ebab102a470
锡爵 <xijue.yp@alibaba-inc.com>
2014-12-10 15:53:32 +0800

    添加说明

--- src/ReorderList/Solution.java
+++ src/ReorderList/Solution.java
@@ -14,11 +14,16 @@ package ReorderList;
  *
  * https://oj.leetcode.com/problems/reorder-list/
  *
+ * Solve：
+ * 1. 问题抽象：把后半部的数据倒序插入进前面
+ * 2. 找到后半部 --> 倒序后半部 --> 插入前半部
+ *
  * Created by xijueyp on 14-12-9.
  */
 public class Solution {
     public void reorderList(ListNode head) {
         if(head==null) return;
+
         ListNode slow = head, fast = head;
         while(fast!=null && fast.next!=null){
             slow = slow.next;
@@ -44,10 +49,8 @@ public class Solution {
             left = right.next;
             right = mid.next;
         }
-
     }
 }
-
 
 class ListNode {
 int val;
