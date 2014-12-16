a594043a71666d245949b01a1ccb9ae917f815a8
锡爵 <xijue.yp@alibaba-inc.com>
2014-12-16 18:03:23 +0800

    add solutation Word break

--- src/MergekSortedLists/Solution.java
+++ src/MergekSortedLists/Solution.java
@@ -1,6 +1,8 @@
 package MergekSortedLists;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Created by xijueyp on 14-12-10.
@@ -15,43 +17,48 @@ import java.util.List;
  *     }
  * }
  *
- * todo
+ * https://oj.leetcode.com/problems/merge-k-sorted-lists/
+ *
+ * 第一次计算：使用列表遍历，超时
+ * 思路：
+ * 时间的消耗有两点:1. 每一轮的排序 2. 一个list当中如果已经没有节点了，但是还是会每一轮都要遍历
+ * 针对上面的问题，首先使用一个最小堆，每一轮的比较就可以变成 log(n)，而且只要是一个list变成null，就不加入最小堆。
+ * 这样上面的效率问题就解决了
+ *
  */
 public class Solution {
-
-    boolean c(int a ,int b){
-        return a<b;
-    }
-
     public ListNode mergeKLists(List<ListNode> lists) {
-        ListNode result = null;
+        if (lists==null||lists.size()==0) return null;
 
-        for (ListNode listNode:lists){
-            if (listNode == null)
-                continue;
-            ListNode index = listNode.next;
-            listNode.next = new ListNode(listNode.getVal(), listNode.getNext());
-            listNode.getNext().setNext(index);
-        }
-
-        while (true){
-            // todo 怎么都需要遍历之后修改 list 中的对应值，所以必须要做的就是 for(;;) 形式的循环
-            // todo 如果是ArrayList的话效率应该不会有什么问题
-            for (ListNode listNode:lists){
-                if (listNode == null) continue;
-
-                if (result == null || c(result.getVal(), listNode.getNext().getVal())){
-                    result = listNode;
-                }
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else
+                    return 1;
             }
+        });
 
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
 
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
 
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+
+            if (tail.next!=null)
+                queue.add(tail.next);
         }
-
+        return dummy.next;
     }
 }
-
 class ListNode {
     int val;
     ListNode next;
--- src/WordBreak/Solution.java
+++ src/WordBreak/Solution.java
@@ -1,23 +1,44 @@
 package WordBreak;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * todo
  * Created by xijueyp on 14-12-10.
+ *
+ * https://oj.leetcode.com/problems/word-break/
+ *
+ * describe:
+ * Given a string s and a dictionary of words dict, determine if s can be segmented into
+ * a space-separated sequence of one or more dictionary words.
+ *
+ * For example, given
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ * Return true because "leetcode" can be segmented as "leet code".
+ *
+ * solution reference: http://blog.csdn.net/linhuanmars/article/details/22358863
  */
 public class Solution {
+
     public boolean wordBreak(String s, Set<String> dict) {
-        return true;
-    }
-
-    public static void main(String ap[]){
-        Set<String> strings = new HashSet<String>();
-
-        strings.add("a");
-        strings.add("s");
-
-        System.out.println(new Solution().wordBreak("ass", strings));
+        if(s==null || s.length()==0)
+            return true;
+        boolean[] res = new boolean[s.length()+1];
+        res[0] = true;
+        for(int i=0;i<s.length();i++)
+        {
+            StringBuilder str = new StringBuilder(s.substring(0,i+1));
+            for(int j=0;j<=i;j++)
+            {
+                if(res[j] && dict.contains(str.toString()))
+                {
+                    res[i+1] = true;
+                    break;
+                }
+                str.deleteCharAt(0);
+            }
+        }
+        return res[s.length()];
     }
 }
